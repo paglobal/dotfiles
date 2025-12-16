dc413e02461072ff73ddd31ab6bf4e7467fc15c8
paglobal <jackamoah.pa@gmail.com>
2025-12-16 20:28:08 +0000

    mako: change high priority noti sound

--- scripts/executable_install-from-pkglist.sh
+++ scripts/executable_install-from-pkglist.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-yay -S --needed - < pkglist.txt
+yay -S --needed - < ~/pkglist.txt
--- scripts/executable_update-pkglist.sh
+++ scripts/executable_update-pkglist.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 yay -Qqe > ~/pkglist.txt
+chezmoi add ~/pkglist.txt
